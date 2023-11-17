65cd0273afe3e78d66368c4b956a28fa7761ac27
khanhhm <hokhanhzxz@gmail.com>
2023-11-17 21:58:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,168 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import * as faceapi from 'face-api.js';
 import './App.css';
+import face from './faceData';
+import Toastify from 'toastify-js';
+import "toastify-js/src/toastify.css";
 
 function App() {
+  const videoHeight = 480;
+  const videoWidth = 640;
+  const [initializing, setInitializing] = useState(false);
+  const videoRef = useRef();
+  const canvasRef = useRef();
+  const faceMatcherRef = useRef();
+  const detectionsRef = useRef([]);
+  const detectionsNameRef = useRef();
+  const detectionsLimitRef = useRef(0);
+
+  const loadTrainingData = async () => {
+    const label = face.name;
+    const faceDescriptors = [];
+    const descriptors = [];
+  
+    // Sử dụng Promise.all để chờ cho tất cả các tác vụ tải hình ảnh và nhận dạng kết thúc
+    await Promise.all(face.images.map(async (imageData) => {
+      try {
+        const image = await faceapi.fetchImage(imageData.src);
+        const detection = await faceapi.detectSingleFace(image).withFaceLandmarks().withFaceDescriptor();
+        descriptors.push(detection.descriptor);
+      } catch (error) {
+        console.error(`Error loading or processing image: ${imageData.src}`);
+      }
+    }));
+  
+    faceDescriptors.push(new faceapi.LabeledFaceDescriptors(label, descriptors));
+
+    Toastify({
+      text: "Đã training data xong...",
+      duration: 2000,
+      close: true,
+      gravity: "top",
+      position: "right",
+      stopOnFocus: true,
+    }).showToast();
+
+    return faceDescriptors;
+  };
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models';
+      setInitializing(true);
+      await Promise.all([
+        faceapi.nets.ssdMobilenetv1.loadFromUri(MODEL_URL),
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+
+      Toastify({
+        text: "Đã load xong models...",
+        duration: 2000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+      }).showToast();
+
+      const trainingData = await loadTrainingData()
+      faceMatcherRef.current = new faceapi.FaceMatcher(trainingData, 0.7)
+
+	    console.log(faceMatcherRef.current)
+    }
+    loadModels();
+  }, [])
+
+  const startVideo = () => {
+    navigator.getUserMedia({
+      video: {}
+    }, stream => videoRef.current.srcObject = stream, 
+    (err) => {
+      console.error(`The following error occurred: ${err.name}`);
+    })
+  }
+
+  const handleVideoOnplay = () => {
+    setInterval( async () => {
+      if (initializing) {
+        setInitializing(false);
+      }
+
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+      const displaySize = {
+        width: videoWidth,
+        height: videoHeight
+      }
+
+      faceapi.matchDimensions(canvasRef.current, displaySize);
+
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withFaceDescriptors();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      detectionsRef.current = detections;
+
+      canvasRef.current.getContext('2d').clearRect(0, 0, videoWidth, videoHeight);
+      for (const detection of resizedDetections) {
+        detectionsNameRef.current = faceMatcherRef.current.findBestMatch(detection.descriptor).label;
+        const drawBox = new faceapi.draw.DrawBox(detection.detection.box, {
+          label: detectionsNameRef.current
+        })
+        drawBox.draw(canvasRef.current)
+      }
+      // faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      // faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+
+      console.log('detectionsRef.current', detectionsRef.current)
+      if (detectionsRef.current.length > 0) {
+        Toastify({
+          text: detectionsNameRef.current,
+          duration: 2000,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+        }).showToast();
+        if (detectionsNameRef.current !== face.name) {
+          detectionsLimitRef.current++;
+          console.log('detectionsLimitRef.current', detectionsLimitRef.current)
+        } else {
+          detectionsLimitRef.current = 0;
+        }
+      } else {
+        Toastify({
+          text: "Không phát hiện khuôn mặt...",
+          duration: 2000,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+        }).showToast();
+        detectionsLimitRef.current++;
+        console.log('detectionsLimitRef.current', detectionsLimitRef.current)
+      }
+
+      if (detectionsLimitRef.current > 10) {
+        Toastify({
+          text: "Bạn đã vượt quá số lần vắng mặt, mời bạn ra khỏi lớp",
+          duration: 2000,
+          close: true,
+          gravity: "top",
+          position: "right",
+          stopOnFocus: true,
+        }).showToast();
+      }
+    }, 5000)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span style={{ fontSize: '24px', color: '#fff' }}>{initializing ? 'Initializing' : 'Ready'}</span>
+      <div className='container'>
+        <video className='video' ref={videoRef} height={videoHeight} width={videoWidth} onPlay={handleVideoOnplay} autoPlay muted />
+        <canvas className='canvas' ref={canvasRef} />
+      </div>
     </div>
   );
 }
---
+++ src/faceData.js
@@ -0,0 +1,21 @@
+const face = {
+    "id": "123456789",
+    "name": "Messi",
+    "images": [
+      {
+        "src": "https://i.guim.co.uk/img/media/add678f52177de8c573c1e474f05acc177dbe6f8/0_106_3178_1907/master/3178.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=494c28738c2d75c1c43886c36d5e3d89",
+      },
+      {
+        "src": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202306/lionel_messi_reuters_psg-three_four.jpg?VersionId=JVfvlxgoCjLDwSk76JF2a5YPBcJY8fEG",
+      },
+      {
+        "src": "https://cdn.tuoitre.vn/471584752817336320/2023/1/2/tumblr659e10cb861fff4e437db987cdfca4ed3232a1ee540-16726344545721610254341.jpg",
+      },
+      {
+        "src": "https://hips.hearstapps.com/hmg-prod/images/lionel-messi-celebrates-after-their-sides-third-goal-by-news-photo-1686170172.jpg?crop=0.66653xw:1xh;center,top&resize=640:*",
+      },
+    ],
+  };
+  
+  export default face;
+  
